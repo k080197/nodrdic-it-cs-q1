24186825a82aaa9fe50098233fe3d4c90e798557
Константин <k080197@mail.ru>
2019-09-20 01:57:30 +0300

    Homework

---
+++ Lessons/02/Homework/HomeWork/HomeWork/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа и операцию над ними (+, -, *, /, остаток от деления %, возведение в степень ^):");
+
+            var s1 = double.Parse(Console.ReadLine());
+            var s2 = double.Parse(Console.ReadLine());
+            var s3 = Console.ReadLine();
+
+            if (s3 == "+")
+            {
+                Console.WriteLine(s1 + s2);
+            }
+            else if (s3 == "-")
+            {
+                Console.WriteLine(s1 - s2);
+            }
+            else if (s3 == "*")
+            {
+                Console.WriteLine(s1 * s2);
+            }
+            else if (s3 == "/")
+            {
+                Console.WriteLine(s1 / s2);
+            }
+            else if (s3 == "%")
+            {
+                Console.WriteLine(s1 % s2);
+            }
+            else if (s3 == "^")
+            {
+                Console.WriteLine(Math.Pow(s1, s2));
+            }
+            else
+            {
+                Console.WriteLine("Ошибка.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
