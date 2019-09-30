eea19343a83c6f96ca9a2a129c57263045f90dea
Константин <k080197@mail.ru>
2019-10-01 00:01:09 +0300

    Исправлены ошибки

--- Lessons/04/Classwork/Classwork4/Classwork4/Program.cs
+++ Lessons/04/Classwork/Classwork4/Classwork4/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 
 namespace Classwork4
 {
@@ -26,22 +26,40 @@ namespace Classwork4
             Console.WriteLine("Enter four color numbers for the palette: ");
 
             for (int i = 0; i < 8; i++)
-                Console.WriteLine($"{(Colors)(0x1 << i)} - {0x1 << i}");
+            {
+                int shiftOperand = 0x1 << i;
+
+                Console.WriteLine($"{(Colors)shiftOperand} - {shiftOperand}");
+            }
 
             for (int i = 0; i < 4; i++)
+            {
                 favoriteColors = favoriteColors | (Colors)ReadNumber(favoriteColors);
+            }
 
             Console.WriteLine("Favorite colors: ");
 
             for (int i = 0; i < 8; i++)
-                if ((favoriteColors & (Colors)(0x1 << i)) == (Colors)(0x1 << i))
-                    Console.WriteLine($"{(Colors)(0x1 << i)} - {0x1 << i}");
+            {
+                int shiftOperand = 0x1 << i;
+
+                if ((favoriteColors & (Colors)shiftOperand) == (Colors)shiftOperand)
+                {
+                    Console.WriteLine($"{(Colors)shiftOperand} - {shiftOperand}");
+                }
+            }
 
             Console.WriteLine("Unfavorite colors: ");
 
             for (int i = 0; i < 8; i++)
-                if ((favoriteColors & (Colors)(0x1 << i)) != (Colors)(0x1 << i))
-                    Console.WriteLine($"{(Colors)(0x1 << i)} - {0x1 << i}");
+            {
+                int shiftOperand = 0x1 << i;
+
+                if ((favoriteColors & (Colors)shiftOperand) != (Colors)shiftOperand)
+                {
+                    Console.WriteLine($"{(Colors)shiftOperand} - {shiftOperand}");
+                }
+            }
 
             Console.ReadKey();
         }
@@ -52,12 +70,14 @@ namespace Classwork4
             {
                 Console.Write($"Enter the number: ");
                 
-                var numbers = new HashSet<int>() { 1, 2, 4, 8, 16, 32, 64, 128 };
+                var numbers = new int[] { 1, 2, 4, 8, 16, 32, 64, 128 };
 
                 if (int.TryParse(Console.ReadLine(), out var operand) && numbers.Contains(operand))
                 {
                     if ((colors & (Colors)operand) != (Colors)operand)
+                    {
                         return operand;
+                    }
                 }
 
                 Console.WriteLine("Wrong value! ");
