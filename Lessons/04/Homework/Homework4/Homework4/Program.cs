322a06832d8b90d7e1711f3cde00add32ba6c04e
Константин <k080197@mail.ru>
2019-10-01 14:19:37 +0300

    Домашняя работа

--- Lessons/04/Homework/Homework4/Homework4/Program.cs
+++ Lessons/04/Homework/Homework4/Homework4/Program.cs
@@ -2,8 +2,9 @@
 
 namespace Homework4
 {
-    enum Containers : byte
+    enum Containers : int
     {
+        None = 0x0,
         One = 0x1,
         Five = 0x1 << 1,
         Twenty = 0x1 << 2
@@ -12,7 +13,67 @@ namespace Homework4
     {
         static void Main(string[] args)
         {
-            
+            var litres = Math.Ceiling(ReadNumber());
+            var countOfContainers = new int[3];
+            var containers = Containers.None;
+
+            while (litres > 0)
+            {
+                if (litres >= 20)
+                {
+                    litres -= 20;
+                    containers |= Containers.Twenty;
+                    countOfContainers[0]++;
+                    continue;
+                }
+                if (litres >= 5)
+                {
+                    litres -= 5;
+                    containers |= Containers.Five;
+                    countOfContainers[1]++;
+                    continue;
+                }
+                if (litres >= 1)
+                {
+                    litres -= 1;
+                    containers |= Containers.One;
+                    countOfContainers[2]++;
+                    continue;
+                }
+            }
+
+            Console.WriteLine($"Вам потребуются следующие контейнеры:");
+
+            if((containers & Containers.Twenty) == Containers.Twenty)
+            {
+                Console.WriteLine($"20 л: {countOfContainers[0]} шт.");
+            }
+            if ((containers & Containers.Five) == Containers.Five)
+            {
+                Console.WriteLine($"5 л: {countOfContainers[1]} шт.");
+            }
+            if ((containers & Containers.One) == Containers.One)
+            {
+                Console.WriteLine($"1 л: {countOfContainers[2]} шт.");
+            }
+
+            Console.ReadKey();
         }
+
+        static decimal ReadNumber()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Какой объем сока (в литрах) требуется упаковать?");
+
+                if (decimal.TryParse(Console.ReadLine(), out var operand))
+                {
+                    return operand;
+                }
+
+                Console.WriteLine("Wrong value! ");
+            }
+        }
+
     }
 }
