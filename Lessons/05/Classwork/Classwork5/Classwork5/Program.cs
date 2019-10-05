2d7a94193af574acff41dc01f863cff671bef139
Константин <k080197@mail.ru>
2019-10-05 20:46:02 +0300

    Классная работа

---
+++ Lessons/05/Classwork/Classwork5/Classwork5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Classwork5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number from 0 to 99:");
+
+            var number = int.Parse(Console.ReadLine());
+            
+            switch(number % 10)
+            {
+                case 1 when number / 10 != 1:
+                    Console.WriteLine($"{number} год.");
+                    break;
+                case 2 when number / 10 != 1:
+                case 3 when number / 10 != 1:
+                case 4 when number / 10 != 1:
+                    Console.WriteLine($"{number} года.");
+                    break;
+                default:
+                    Console.WriteLine($"{number} лет.");
+                    break;
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
