056403a6ae713c325e32c3a742a8ef807826f417
Константин <k080197@mail.ru>
2019-10-09 16:23:19 +0300

    Самостоятельная работа 1

--- Lessons/06/Classwork/Classwork/Classwork/Program.cs
+++ Lessons/06/Classwork/Classwork/Classwork/Program.cs
@@ -6,11 +6,40 @@ namespace Classwork
     {
         static void Main(string[] args)
         {
-            do
+            var marks = new[]
             {
-                Console.WriteLine("Введите exit для выхода");
+                new [] { 2, 3, 3, 2, 3}, // Monday (it was a good weekend :)
+                new [] { 2, 4, 5, 3}, // Tuesday (anyway better than Monday)
+                null, // Wednesday (felt sick, stayed at home :( )
+                new [] { 5, 5, 5, 5}, // Thursday (God mode :)
+                new [] { 4 } // Friday (a very short day)
+            };            var numberOfMarks = 0.0;
+            var totalSum = 0.0;
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                var marksSum = 0.0;
+
+                if (marks[i] == null)
+                {
+                    Console.WriteLine($"The average mark for {(DayOfWeek)(i + 1)} - N/A");
+                    continue;
+                }
+
+                for (int j = 0; j < marks[i].Length; j++)
+                {
+                    marksSum += marks[i][j];
+                    totalSum += marks[i][j];
+                    numberOfMarks++;
+                }
+
+                var average = marksSum / marks[i].Length;
+
+                Console.WriteLine("The average mark for {0} - {1:0.0}", (DayOfWeek)(i + 1), average);
             }
-            while (string.Equals(Console.ReadLine(), "exit", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine("Average rating for week - {0:0.0}", totalSum / numberOfMarks);
+            Console.ReadKey();
         }
     }
 }
