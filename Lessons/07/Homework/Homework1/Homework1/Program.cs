fe288b42016079778a509a339bd6b0e5237c49ad
Константин <k080197@mail.ru>
2019-10-14 17:03:58 +0300

    Домашняя работа 1

---
+++ Lessons/07/Homework/Homework1/Homework1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstSymbolCounter(Console.ReadLine(), 'a'));
+
+            Console.ReadKey();
+        }
+
+        private static string DeleteSpaces(string text)
+        {
+            var result = new StringBuilder();
+
+            for (int i = 0, p = 0; i < text.Length; p = i++)
+            {
+                var current = text[i];
+                var previous = text[p];
+
+                if (char.IsWhiteSpace(current) &&
+                    char.IsLetter(previous))
+                {
+                    result.Append(' ');
+                }
+            }
+            Console.WriteLine(result);
+            return result.ToString();
+        }
+
+        private static string FirstSymbolCounter(string text, char symbol)
+        {
+            var word = text[0] == symbol ? 1 : 0;
+
+            for (int i = 0, p = 0; i < text.Length; p = i++)
+            {
+                var current = text[i];
+                var previous = text[p];
+                Console.WriteLine(i +" "+ word);
+                if (char.IsWhiteSpace(previous) &&
+                    current == symbol)
+                {
+                    word++;
+                } 
+            }
+
+            if (word > 0)
+            {
+                return $"{word} слов начинаются с буквы {symbol} {text}";
+            }
+
+            return "Вы ввели мало слов.";
+        }
+    }
+}
