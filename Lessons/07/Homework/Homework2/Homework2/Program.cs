3e8e26685585de51269e87b94112630b2fe6ec76
Константин <k080197@mail.ru>
2019-10-15 13:54:19 +0300

    Домашнее задание 2

---
+++ Lessons/07/Homework/Homework2/Homework2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите непустую строку:");
+
+            var text = new StringBuilder();
+            text.Append(Console.ReadLine());
+
+            while (string.IsNullOrWhiteSpace(text.ToString()))
+            {
+                Console.WriteLine("Вы ввели пустую строку :( Попробуйте ещё раз:");
+                text.Append(Console.ReadLine());
+            }
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                Console.Write(char.ToLower(text.ToString()[i]));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
