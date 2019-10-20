8a7d0eeefddcb6d473150a1a050b675c578808e1
Константин <k080197@mail.ru>
2019-10-20 17:24:38 +0300

    Исправлены замечания

--- Lessons/07/Homework/Homework2/Homework2/Program.cs
+++ Lessons/07/Homework/Homework2/Homework2/Program.cs
@@ -7,16 +7,8 @@ namespace Homework2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите непустую строку:");
-
             var text = new StringBuilder();
-            text.Append(Console.ReadLine());
-
-            while (string.IsNullOrWhiteSpace(text.ToString()))
-            {
-                Console.WriteLine("Вы ввели пустую строку :( Попробуйте ещё раз:");
-                text.Append(Console.ReadLine());
-            }
+            text.Append(InputText());
 
             for (int i = text.Length - 1; i >= 0; i--)
             {
@@ -24,6 +16,27 @@ namespace Homework2
             }
 
             Console.ReadKey();
+        }
+
+        static string InputText()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите непустую строку:");
+                    var text = Console.ReadLine();
+
+                    if (!string.IsNullOrWhiteSpace(text))
+                    {
+                        return text;
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("Вы ввели пустую строку!");
+                }
+            }
         }
     }
 }
