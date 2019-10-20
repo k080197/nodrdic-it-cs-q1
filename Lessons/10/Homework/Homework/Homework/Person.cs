c4abd5487eacf88f2cb7052a18d529f62a1555a6
Константин <k080197@mail.ru>
2019-10-21 01:39:23 +0300

    Домашняя работа

---
+++ Lessons/10/Homework/Homework/Homework/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    class Person
+    {
+        public string Name { get; set; }
+        private byte m_age;
+        public byte Age
+        {
+            get
+            {
+                return m_age;
+            }
+            set
+            {
+                if (value < 0 && value > 120)
+                {
+                    throw new ArgumentException("Вы ввели неправильный возраст.");
+                }
+                m_age = value;
+            }
+        }
+        private byte AgeAfter4Years => 
+            (byte)(m_age + 4);
+        public string Description =>
+            $"Name: {Name}, age in 4 years: {AgeAfter4Years}";
+    }
+}
---
+++ Lessons/10/Homework/Homework/Homework/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new Person[3];
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                persons[i] = new Person();
+                persons[i].Name = InputName(i);
+                persons[i].Age = InputAge(i);
+            }
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                Console.WriteLine(persons[i].Description);
+            }
+
+            Console.ReadKey();
+        }
+
+        static string InputName(int index)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Input {index + 1} name:");
+                var text = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(text))
+                {
+                    return text;
+                }
+
+                Console.WriteLine("Wrong value!");
+            }
+        }
+
+        static byte InputAge(int index)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Input {index + 1} age:");
+
+                if (byte.TryParse(Console.ReadLine(), out var operand))
+                {
+                    if (operand > 0 && operand < 120)
+                    {
+                        return operand;
+                    }
+                }
+
+                Console.WriteLine("Wrong value!");
+            }
+        }
+    }
+}
