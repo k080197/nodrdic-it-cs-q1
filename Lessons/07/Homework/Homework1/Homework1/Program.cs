e0f30cdda7ebe107c7d0069b83a9de306b28627a
Константин <k080197@mail.ru>
2019-10-20 21:10:42 +0300

    Исправлены замечания

--- Lessons/07/Homework/Homework1/Homework1/Program.cs
+++ Lessons/07/Homework/Homework1/Homework1/Program.cs
@@ -7,22 +7,17 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
-            var symbol = 'A'; // ENG
-            var text = InputText();
-
-            Console.WriteLine($"Количество слов, начинающихся с буквы '{symbol}': {CountFirstSymbol(text, symbol)}.");
+            Console.WriteLine($"Количество слов, начинающихся с буквы 'A': {CountFirstSymbol(InputText(), 'A')}.");
             Console.ReadKey();
         }
 
-        private static int CountFirstSymbol(string text, char symbol)
+        private static int CountFirstSymbol(string[] words, char symbol)
         {
-            text = text.ToUpper();
             var count = 0;
-            var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
             foreach (string word in words)
             {
-                if (word[0] == symbol)
+                if (word[0] == symbol || word[0] == char.ToLower(symbol))
                 {
                     count++;
                 }
@@ -31,43 +26,21 @@ namespace Homework1
             return count;
         }
 
-        private static int CountWords(string text)
-        {
-
-            var count = 1;
-
-            for (int i = 0, p = 0; i < text.Length; p = i++)
-            {
-                var current = text[i];
-                var previous = text[p];
-
-                if (char.IsWhiteSpace(previous) &&
-                    char.IsLetter(current))
-                {
-                    count++;
-                }
-            }
-
-            return count;
-        }
-
-        static string InputText()
+        static string[] InputText()
         {
             while (true)
             {
-                try
-                {
-                    Console.WriteLine("Введите строку из нескольких слов: ");
-                    var text =  Console.ReadLine();
+                Console.WriteLine("Введите строку из нескольких слов: ");
+                var text =  Console.ReadLine();
 
-                    if (CountWords(text) > 1)
-                    {
-                        return text;
-                    }
-                }
-                catch (NullReferenceException)
+                if (!string.IsNullOrEmpty(text))
                 {
-                    Console.WriteLine("Вы ввели пустую строку!");
+                    var textArray = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    if (textArray.Length > 1)
+                    {
+                        return textArray;
+                    }
                 }
             }
         }
