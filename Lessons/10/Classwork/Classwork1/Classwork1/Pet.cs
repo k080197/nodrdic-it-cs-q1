b54f50e9c63d4a8e278e2ab4dc42afeeae58ae33
Константин <k080197@mail.ru>
2019-10-20 23:35:31 +0300

    Классная работа

---
+++ Lessons/10/Classwork/Classwork1/Classwork1/Pet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Classwork1
+{
+    enum Kind: byte
+    {
+        Mouse,
+        Cat,
+        Dog,
+        Bird
+    }
+
+    enum Sex : byte
+    {
+        F,
+        M
+    }
+    class Pet
+    {
+        public Kind Kind { get; set; }
+
+        public string Name { get; set; }
+
+        public Sex Sex { get; set; }
+
+        public int Age { get; set; }
+        public Pet()
+        {
+
+        }
+
+        public string Description =>
+            $"Вид: {Kind}, Имя: {Name}, Пол: {Sex}, Возраст: {Age}";
+
+    }
+}
---
+++ Lessons/10/Classwork/Classwork1/Classwork1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Classwork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pet1 = new Pet()
+            {
+                Kind = Kind.Bird,
+                Name = "Kesha",
+                Sex = Sex.M,
+                Age = 10
+            };
+
+            var pet2 = new Pet();
+            pet2.Kind = Kind.Dog;
+            pet2.Name = "The ball";
+            pet2.Sex = Sex.F;
+            pet2.Age = 12;
+
+            Console.WriteLine(pet1.Description);
+            Console.WriteLine(pet2.Description);
+
+            Console.ReadKey();
+        }
+    }
+}
