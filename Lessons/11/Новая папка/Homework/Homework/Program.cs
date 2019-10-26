a6841f6e5d87f31afd74be055c100cfefa00a630
Константин <k080197@mail.ru>
2019-10-26 14:20:57 +0300

    Домашняя работа

---
+++ Lessons/11/Новая папка/Homework/Homework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reminderItem1 = new ReminderItem(new DateTime(2019, 10, 10, 6, 30, 15), "Будильник 1");
+            var reminderItem2 = new ReminderItem(new DateTime(2019, 11, 10, 6, 30, 15), "Будильник 2");
+
+            reminderItem1.WriteProperties();
+            reminderItem2.WriteProperties();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lessons/11/Новая папка/Homework/Homework/ReminderItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    class ReminderItem
+    {
+        public DateTime AlarmDate { get; set; }
+        public string AlarmMessage { get; set; }
+        public TimeSpan TimeToAlarm =>
+            DateTime.Now - AlarmDate;
+        public bool IsOutdated =>
+            TimeToAlarm.Milliseconds < 0 ? false : true;
+
+        public ReminderItem(DateTime alarmDate, string alarmMessage)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+
+        public void WriteProperties()
+        {
+            Console.WriteLine($"Alarm date: {AlarmDate}");
+            Console.WriteLine($"Alarm message: {AlarmMessage}");
+            Console.WriteLine($"Time to alarm: {TimeToAlarm}");
+            Console.WriteLine($"Is outdated: {IsOutdated}");
+        }
+    }
+}
