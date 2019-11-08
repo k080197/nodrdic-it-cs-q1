56cb1cb7c643aa8f79df4d7f30eb840c95af269d
Константин <k080197@mail.ru>
2019-11-08 22:51:51 +0300

    Classwork

---
+++ Lessons/14/Classwork/Classwork/Classwork/ErrorList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classwork
+{
+    class ErrorList : IDisposable, IEnumerable<string>
+    {
+        public readonly string Category;
+        public List<String> Errors { get; set; }
+        public void Dispose()
+        {
+            Errors.Clear();
+            Errors = null;
+        }
+        public ErrorList(string category, List<string> errors)
+        {
+            Category = category;
+            Errors = errors;
+        }
+        public void Add(string errorMessage)
+        {
+            Errors.Add(errorMessage);
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return Errors.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Lessons/14/Classwork/Classwork/Classwork/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfErrors = new List<string>();
+
+            using (ErrorList errorList = new ErrorList("Errors", listOfErrors))
+            {
+                errorList.Add("Ошибка 1");
+                errorList.Add("Ошибка 2");
+                errorList.Add("Ошибка 3");
+
+                foreach(var errors in errorList)
+                {
+                    Console.WriteLine(errorList.Category + " : " + errors);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
