1a6da268a56dd5923bb6000d0beaf691f7bf1e24
Константин <k080197@mail.ru>
2019-11-10 22:19:59 +0300

    Исправлены ошибки

--- Lessons/15/Homework/ConsoleApp1/ConsoleApp1/LogWriterFactory.cs
+++ Lessons/15/Homework/ConsoleApp1/ConsoleApp1/LogWriterFactory.cs
@@ -6,25 +6,32 @@ namespace ConsoleApp1
 {
     class LogWriterFactory
     {
-        private static ILogWriter instance;
+        private static LogWriterFactory instance;
         private LogWriterFactory()
         {
         }
-        public static ILogWriter GetLogWriter<T>(object parameters = null) where T : ILogWriter, new()
+        public static LogWriterFactory Instance
+        {
+            get
+            {
+                return instance ?? (instance = new LogWriterFactory());
+            }
+        }
+        public ILogWriter GetLogWriter<T>(object parameters = null) where T : ILogWriter, new()
         {
             if (typeof(T) == typeof(ConsoleLogWriter))
             {
-                return instance ?? (instance = new ConsoleLogWriter());
+                return new ConsoleLogWriter();
             }
             if (typeof(T) == typeof(FileLogWriter))
             {
-                return instance ?? (instance = new FileLogWriter(parameters.ToString()));
+                return new FileLogWriter(parameters.ToString());
             }
             if (typeof(T) == typeof(MultipleLogWriter)) 
             {
-                return instance ?? (instance = new MultipleLogWriter((ILogWriter[])parameters));
+                return new MultipleLogWriter((ILogWriter[])parameters);
             }
-            return instance;
+            return null;
         }        
         
     }
--- Lessons/15/Homework/ConsoleApp1/ConsoleApp1/Program.cs
+++ Lessons/15/Homework/ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,19 +7,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            //var logWriterFactory1 = LogWriterFactory.Instance.GetLogWriter<ConsoleLogWriter>();
             //var logWriterFactory1 = LogWriterFactory.GetLogWriter<ConsoleLogWriter>();
 
             //var logWriterFactory2 = LogWriterFactory.GetLogWriter<FileLogWriter>("log.log");
 
-            var logWriterFactory3 = LogWriterFactory.GetLogWriter<MultipleLogWriter>(
+            var logWriterFactory3 = LogWriterFactory.Instance.GetLogWriter<MultipleLogWriter>(
                 new ILogWriter[]{
                     new ConsoleLogWriter(),
                     new ConsoleLogWriter(),
                     new ConsoleLogWriter(),
                     new ConsoleLogWriter(),
-                    new FileLogWriter("log1.log"),
-                    new FileLogWriter("log2.log")
+                    new FileLogWriter("log3.log"),
+                    new FileLogWriter("log4.log")
                 }
             );
 
